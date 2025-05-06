9da95b740c2ef3789990d0d18763e668c43acebc
DavieDevs <199727366+DavieDevs@users.noreply.github.com>
2025-05-05 20:34:38 -0500

    feat: added brand mark (#2)
    Co-authored-by: Damian Padilla <damianpadilla@Mac.lan>

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="absolute ">
+    <div className="relative w-full h-screen overflow-hidden">
       <video
-        src="/Parallax_2024_HL.mp4"
+        src="/parallaxclips.mp4"
         autoPlay
         muted
         playsInline
         loop
-        className="top-0 left-0 w-full h-full object-cover z-0 -rotate-90"
+        className="absolute top-0 left-0 w-full h-full object-cover"
       />
-      <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-10 -rotate-90" />
+      <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-10 onject-cover" />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
         <Image
           src="/Print_Primary_Lace.png"
