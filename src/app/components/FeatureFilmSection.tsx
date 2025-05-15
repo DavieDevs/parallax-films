966d05e15d1e9a60e40713922f0915382eb7ef83
DavieDevs <199727366+DavieDevs@users.noreply.github.com>
2025-05-15 18:14:48 -0500

    feat: added film component for videos (#4)
    Co-authored-by: Damian Padilla <damianpadilla@Mac.lan>

--- src/app/components/FeatureFilmSection.tsx
+++ src/app/components/FeatureFilmSection.tsx
@@ -1,12 +1,18 @@
 import Button from "../components/Button";
+import Film from "./Film";
 import LABELS from "../constants/labels";
 
 export default function FeatureFilmSection() {
   return (
-    <div className="bg-sand w-full h-screen flex flex-col justify-center items-center">
+    <div className="bg-sand w-full  flex flex-col justify-center items-center">
       <h2 className="text-mosswood font-bold p-4 text-3xl">
         {LABELS.featureFilm.title}
       </h2>
+      <div className="p-4 flex flex-col w-full">
+        <Film videoId="1004015252" />
+        <Film videoId="1019498597" />
+        <Film videoId="1004931664" />
+      </div>
       <Button />
     </div>
   );
