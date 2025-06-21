d05776ec2a775599477f90d5ced12e712de67f7d
Damian Padilla <damianpadilla@Mac.lan>
2025-06-21 00:14:15 -0500

    feat:added bio section

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,9 +1,28 @@
 import LABELS from "../constants/labels";
 
-export default function Button() {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: "mosswood" | "sand" | "primary" | "secondary" | "accent";
+  className?: string;
+}
+
+export default function Button({
+  className = "",
+  children,
+  color = "primary",
+  ...props
+}: ButtonProps) {
+  const colorStyles: Record<string, string> = {
+    primary: "bg-primary text-white hover:brightness-110",
+    secondary: "bg-secondary text-white hover:brightness-110",
+    accent: "bg-accent text-white hover:brightness-110",
+    sand: "bg-sand text-black hover:brightness-110",
+  };
+
+  const base =
+    "px-4 py-2 rounded-md font-semibold transition duration-300 shadow-xl";
   return (
-    <button className="bg-mosswood text-white px-4">
-      {LABELS.button.viewFullPortfolio}
+    <button className={`${className} ${colorStyles[color]} ${base}`} {...props}>
+      {children}
     </button>
   );
 }
