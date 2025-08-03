6e0592fe5d63b7a776c9347d4b65d3de10ca2dfa
Damian Padilla <damianpadilla@Mac.lan>
2025-08-03 02:31:08 -0500

    fix: added link to logo for navigation (PF-14-logo-route)

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import { Instagram, Youtube } from "lucide-react";
 import ZolaBadge from "./ZolaBadge";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="bg-mosswood p-4 flex flex-col items-center justify-between">
       <div className="flex flex-row w-full">
-        <Image
-          src="/Print_LogoVariation_Lace.png"
-          alt="Logo"
-          width={150}
-          height={150}
-          className="rounded-md"
-        />
+        <Link href={"/"}>
+          <Image
+            src="/Print_LogoVariation_Lace.png"
+            alt="Logo"
+            width={150}
+            height={150}
+            className="rounded-md"
+          />
+        </Link>
 
         <div className="flex flex-col items-end gap-4 w-full p-4">
           <span className="text-sand">Follow Us</span>
--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navigation() {
   return (
     <div className="absolute top-8 left-8 z-20">
-      <Image
-        src="/Print_LogoVariation_Lace.png"
-        alt="Logo"
-        width={150}
-        height={150}
-        className="rounded-md"
-      />
+      <Link href={"/"}>
+        <Image
+          src="/Print_LogoVariation_Lace.png"
+          alt="Logo"
+          width={150}
+          height={150}
+          className="rounded-md"
+        />
+      </Link>
     </div>
   );
 }
